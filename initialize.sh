e70fb4cb11e74a5e5fac95d1d4b17916f76322ae
Anoop-cs011 <120665542+Anoop-cs011@users.noreply.github.com>
2025-04-16 19:06:00 +0530

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,7 @@
 #! bin/bash
 sudo apt update && sudo apt -y install git python3-pip virtualenv
-git clone https://github.com/Anoop-cs011/VCC-assignment3.git
-cd VCC-assignment3
+git clone https://github.com/Anoop-cs011/VCC-Project.git
+cd VCC-Project
 virtualenv myProjectEnv
 source myProjectEnv/bin/activate
 pip install -r requirements.txt
