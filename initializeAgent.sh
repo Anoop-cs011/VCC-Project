6a72412f3f1d2ad81983d7c284f18a061aa382a7
Anoop-cs011 <120665542+Anoop-cs011@users.noreply.github.com>
2025-04-19 00:18:36 +0530

    Update initializeAgent.sh

--- initializeAgent.sh
+++ initializeAgent.sh
@@ -1,9 +1,5 @@
 #! bin/bash
-sudo apt update && sudo apt -y install git python3-pip virtualenv unzip apt-transport-https ca-certificates gnupg curl
-echo "deb http://packages.cloud.google.com/apt gcsfuse-focal main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt update && sudo apt install -y gcsfuse
-mkdir -p ~/gcs_bucket
+sudo apt update && sudo apt -y install git python3-pip virtualenv
 git clone https://github.com/Anoop-cs011/VCC-Project.git
 cd VCC-Project
 virtualenv myProjectEnv
