8df21527fa1bebf5c4a046d26518c3df019087c3
Anoop-cs011 <120665542+Anoop-cs011@users.noreply.github.com>
2025-04-18 08:42:29 +0530

    Update initializeAppServer.sh

--- initializeAppServer.sh
+++ initializeAppServer.sh
@@ -1,7 +1,6 @@
 #! bin/bash
-sudo apt update && sudo apt -y install git python3-pip virtualenv curl unzip google-cloud-cli
-gcloud auth login
-gcloud config set project vcc-course-project-457009
+sudo apt update && sudo apt -y install git python3-pip virtualenv curl unzip
+
 echo "deb http://packages.cloud.google.com/apt gcsfuse-focal main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt update && sudo apt install -y gcsfuse
